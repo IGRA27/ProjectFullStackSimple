e3e7a87743c81175669e04800017d615ab54167e
IGRA27 <isaac.reyes@epn.edu.ec>
2024-04-17 22:05:48 -0500

    Docker Compose, Volumen, Mongo, Networks & Persistencia | Deploy - Comprobacion

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+from pymongo import MongoClient
+from flask_cors import CORS # pip install Flask-Cors
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+CORS(app)
+client = MongoClient("mongo", 27017) # Nombre del servicio MongoDB en docker-compose.yml
+db = client.sampledb  # Nombre de la base de datos
+collection = db.datacollection  # Nombre de la colección
+
+@app.route('/store', methods=['POST'])
+def store_data():
+    data = request.json['data']
+    collection.insert_one({"data": data})
+    return 'Data stored in MongoDB', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
